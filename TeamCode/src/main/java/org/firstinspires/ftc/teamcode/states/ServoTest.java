1206968de3082072475b1ae03674fbb2980de205
Kai Fu <kaifu7k@gmail.com>
2024-11-16 11:29:55 -0500

    Created ServoTest.java
    testing servo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/states/ServoTest.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.states;
+
+import org.firstinspires.ftc.teamcode.robot.Robot;
+import org.firstinspires.ftc.teamcode.interfaces.IRobot;
+import org.firstinspires.ftc.teamcode.wrappers.JoystickWrapper;
+
+public class ServoTest {
+
+}
