fe1a698b81f4c14d9924768e32bf61b799f688fc
Kai Fu <kaifu7k@gmail.com>
2024-11-16 11:42:08 -0500

    Updated ServoTest.java
    It is updated to test the claw servos, can change to different buttons.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/states/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/states/ServoTest.java
@@ -6,4 +6,26 @@ import org.firstinspires.ftc.teamcode.wrappers.JoystickWrapper;
 
 public class ServoTest {
 
+    private final JoystickWrapper joystick;
+    private static final double CLAW_SERVO_UP = 0.3;
+    private static final double CLAW_SERVO_DOWN = 0.7;
+    private static final double ROT_SERVO = 0.5;
+
+    public ServoTest(JoystickWrapper joystick) {
+        this.joystick = joystick;
+    }
+    
+    public void execute(Robot robot) {
+        if (joystick.gamepad1GetB()) {
+            robot.setClawPosition(CLAW_SERVO_DOWN);
+        } else if (joystick.gamepad1GetX()) {
+            robot.setClawRotationPosition(ROT_SERVO);
+        } else if (joystick.gamepad1GetA()) {
+            robot.setClawPosition(CLAW_SERVO_UP);
+        }else if (joystick.gamepad1GetDUp()) {
+            robot.setClawAnglePosition(0);
+        } else if (joystick.gamepad1GetDDown()) {
+            robot.setClawAnglePosition(1);
+        }
+    }
 }
