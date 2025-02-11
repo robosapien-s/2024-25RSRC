8b603cff4fef545c88b0fe6e81e40890c08c5ad4
williams104 <67794864+williams104@users.noreply.github.com>
2025-02-11 10:24:05 -0500

    MASSIVE CHANGES

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ServoTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.wrappers.JoystickWrapper;
 
