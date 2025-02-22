f5b1fda584dae22efcb54f9010c98e959448bcc8
Kabir <kabirgoyal@icloud.com>
2025-02-22 17:50:05 -0500

    Update rightSideAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/rightSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/rightSideAuto.java
@@ -20,24 +20,24 @@ import org.firstinspires.ftc.teamcode.wrappers.JoystickWrapper;
 @Autonomous
 public class rightSideAuto extends LinearOpMode {
 
-    public static double firstDropY = -42;
+    public static double firstDropY = -41.75;
 
     public static double pickup1x = 25.25;
     public static double pickup1y = -41;
 //    public static double pickup1angle;
 
-    public static double pickup2x  = 30.50;
+    public static double pickup2x  = 30;
     public static double pickup2y = -36.75;
 //    public static double pickup2angle;
 
     public static double pickup3x = 42;
-    public static double pickup3y = -37.25;
+    public static double pickup3y = -36;
 //    public static double pickup3angle;
 
-    public static double wallPickup1x = 32.5;
+    public static double wallPickup1x = 33;
     public static double wallPickup1y = -63;
 
-    public static double wallPickup2x = 33;
+    public static double wallPickup2x = 33.75;
     public static double wallPickup2y = -63.25;
 
     @Override
@@ -49,7 +49,7 @@ public class rightSideAuto extends LinearOpMode {
 
         TrajectoryActionBuilder trajectory1 = drive.actionBuilder(initPose)
                 .stopAndAdd(robotAuto.setState(IRobot.State.SPECIMEN_HANG))
-                .strafeToConstantHeading(new Vector2d(-2, -42))
+                .strafeToConstantHeading(new Vector2d(-2, firstDropY))
                 .waitSeconds(.05)
 //                .stopAndAdd(robotAuto.openTopClawAction())
 //                .waitSeconds(.1)
@@ -94,28 +94,28 @@ public class rightSideAuto extends LinearOpMode {
                 .stopAndAdd(robotAuto.setState(IRobot.State.SPECIMEN_HANG))
                 .waitSeconds(.2)
                 .strafeToConstantHeading(new Vector2d(7,-40.25))//1st drop
-                .waitSeconds(.15)
+                .waitSeconds(.1)
                 .stopAndAdd(robotAuto.setState(IRobot.State.WALLPICKUP))
                 .strafeToConstantHeading(new Vector2d(wallPickup2x,wallPickup2y))
                 .waitSeconds(.1)
                 .stopAndAdd(robotAuto.setState(IRobot.State.SPECIMEN_HANG))
-                .waitSeconds(.25)
+                .waitSeconds(.2)
                 .strafeToConstantHeading(new Vector2d(8.6,-40.25))//2nd drop
-                .waitSeconds(.15)
+                .waitSeconds(.1)
                 .stopAndAdd(robotAuto.setState(IRobot.State.WALLPICKUP))
-                .strafeToConstantHeading(new Vector2d(wallPickup2x,wallPickup2y))
+                .strafeToConstantHeading(new Vector2d(wallPickup2x+.15,wallPickup2y))
                 .waitSeconds(.1)
                 .stopAndAdd(robotAuto.setState(IRobot.State.SPECIMEN_HANG))
-                .waitSeconds(.25)
+                .waitSeconds(.2)
                 .strafeToConstantHeading(new Vector2d(10.35,-40.25))//3rd drop
-                .waitSeconds(.15)
+                .waitSeconds(.1)
                 .stopAndAdd(robotAuto.setState(IRobot.State.WALLPICKUP))
-                .strafeToConstantHeading(new Vector2d(wallPickup2x,wallPickup2y))
-                .waitSeconds(.15)
+                .strafeToConstantHeading(new Vector2d(wallPickup2x+.3,wallPickup2y))
+                .waitSeconds(.1)
                 .stopAndAdd(robotAuto.setState(IRobot.State.SPECIMEN_HANG))
-                .waitSeconds(.25)
+                .waitSeconds(.2)
                 .strafeToConstantHeading(new Vector2d(11.85,-40.25))//4th drop
-                .waitSeconds(.15)
+                .waitSeconds(.1)
                 .stopAndAdd(robotAuto.setState(IRobot.State.WALLPICKUP))
                 .strafeToConstantHeading(new Vector2d(33,-63));
 
